b5995c1305e18f1e403aa0980bec46dd3c125ed1
clhommedet <clhommedet@pictet-technologies.com>
2022-07-22 15:29:41 +0200

    Projet finit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,13 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
